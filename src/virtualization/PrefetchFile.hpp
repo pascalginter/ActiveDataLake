64b321821e8e90296890ece47fc6cfa3c662b7c5
pascalginter <ginpa2000@gmail.com>
2025-02-24 14:33:38 +0100

    Add PrefetchFile

---
+++ src/virtualization/PrefetchFile.hpp
@@ -0,0 +1,55 @@
+#include <aws/core/Aws.h>
+#include <aws/s3-crt/S3CrtClient.h>
+#include <aws/s3-crt/model/GetObjectRequest.h>
+#include <aws/s3-crt/model/HeadObjectRequest.h>
+#include <aws/s3-crt/S3CrtErrors.h>
+
+#include "VirtualizedFile.hpp"
+
+class PrefetchFile : public VirtualizedFile {
+    std::string key;
+    std::atomic<int> outstandingRequests = 0;
+    constexpr const static std::string bucket = "adl-tpch";
+    constexpr const static size_t increment = 16 * (1 << 20);
+    static std::shared_ptr<std::string> result;
+    static thread_local Aws::S3Crt::S3CrtClient client;
+public:
+    explicit PrefetchFile(const std::string& p) : key(p){
+        std::cout << "created remote file" << std::endl;
+    }
+
+    size_t size() override {
+        Aws::S3Crt::Model::HeadObjectRequest request;
+        request.SetBucket(bucket);
+        request.SetKey(key);
+        size_t size = client.HeadObject(request).GetResult().GetContentLength();
+        result->resize(size);
+        for (size_t i = 0; i<size; i+= increment){
+           Aws::S3Crt::Model::GetObjectRequest getRequest;
+           getRequest.SetBucket(bucket);
+           getRequest.SetKey(key);
+           getRequest.SetRange(S3InterfaceUtils::ByteRange(i, std::min(size-1, i+increment)).toRangeString());
+           outstandingRequests += 1;
+           client.GetObjectAsync(getRequest, [this, i](
+                   const Aws::S3Crt::S3CrtClient*,
+                   const Aws::S3Crt::Model::GetObjectRequest& request,
+                   Aws::S3Crt::Model::GetObjectOutcome outcome,
+                   const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
+                       std::stringstream ss;
+                       ss << outcome.GetResult().GetBody().rdbuf();
+                       std::string res = ss.str();
+                       for (size_t j=0; j!=res.size(); j++) (*result)[i + j] = res[j];
+                       outstandingRequests -= 1;
+           });
+        }
+        return size;
+    }
+
+    std::shared_ptr<std::string> getRange(S3InterfaceUtils::ByteRange byteRange) override {
+        outstandingRequests.wait(0);
+        std::shared_ptr<std::string> res;
+        res->resize(byteRange.size());
+        memcpy(res->data(), result->data() + byteRange.begin, byteRange.size());
+        return res;
+    }
+};
