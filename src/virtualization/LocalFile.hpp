1ac6f90e21efdc6dff0270c272162e141c67b014
pascalginter <ginpa2000@gmail.com>
2025-03-13 09:13:13 +0100

    Use mmap in LocalFile

--- src/virtualization/LocalFile.hpp
+++ src/virtualization/LocalFile.hpp
@@ -1,27 +1,64 @@
+#pragma once
 #include <fstream>
-#include <filesystem>
-#include <vector>
+#include <fcntl.h>
+#include <sys/mman.h>
+
 #include "VirtualizedFile.hpp"
 
 inline thread_local std::ifstream file;
 
-class LocalFile final : public VirtualizedFile {
-    std::filesystem::path path;
-    static thread_local std::shared_ptr<std::string> result;
+class MemoryMappedFile{
+    int file;
+    void* data;
+
 public:
-    explicit LocalFile(const std::string& p) : path(p){
+    size_t size;
+    explicit MemoryMappedFile(const char* fileName){
+
+        file = open(fileName, O_RDONLY);
+        if (file < 0){
+            std::cout << "file < 0" << std::endl;
+            exit(2);
+        }
+        size = lseek(file, 0, SEEK_END);
+        data = mmap(nullptr, size, PROT_READ, MAP_SHARED, file, 0);
+        madvise(data, size, MADV_SEQUENTIAL);
+        madvise(data, size, MADV_WILLNEED);
+        if (data == MAP_FAILED){
+            std::cout << "MAP_FAILED" << std::endl;
+            exit(3);
+        }
+    }
+    ~MemoryMappedFile(){
+        munmap(data, size);
+        close(file);
+    }
+
+    const char* begin() const{
+        return static_cast<char*>(data);
+    }
+
+    const char* end() const {
+        return  static_cast<char*>(data) + size;
+    }
+};
+
+
+class LocalFile final : public VirtualizedFile {
+    static thread_local std::shared_ptr<std::string> result;
+    MemoryMappedFile file;
+public:
+    explicit LocalFile(const std::string& p) : file(p.c_str()) {
         std::cout << "created local file" << std::endl;
     }
 
     size_t size() override {
-        return std::filesystem::file_size(path);
+        return file.size;
     }
 
     std::shared_ptr<std::string> getRange(S3InterfaceUtils::ByteRange byteRange) override {
         result->resize(byteRange.size());
-        if (!file.is_open()) file.open(path);
-        file.seekg(byteRange.begin);
-        file.read(result->data(), byteRange.size());
+        memcpy(result->data(), file.begin() + byteRange.begin, byteRange.size());
         return result;
     }
 };
