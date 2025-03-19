0fd76ebf37e472a17bb402e6c98678269554ca8c
pascalginter <ginpa2000@gmail.com>
2025-03-19 09:27:33 +0100

    Fetch file footer first

--- src/virtualization/PrefetchFile.hpp
+++ src/virtualization/PrefetchFile.hpp
@@ -8,40 +8,47 @@
 class PrefetchFile final : public VirtualizedFile {
     std::string key;
     std::array<std::atomic<bool>, 1024> outstandingRequests;
+    size_t size_ = 0;
     constexpr static std::string bucket = "adl-tpch";
     constexpr static size_t increment = 16 * (1 << 20);
     static std::shared_ptr<std::string> result;
-    static Aws::S3Crt::S3CrtClient client;
+    Aws::S3Crt::S3CrtClient client;
+
+    void makeRequest(size_t i) {
+        Aws::S3Crt::Model::GetObjectRequest getRequest;
+        getRequest.SetBucket(bucket);
+        getRequest.SetKey(key);
+        const size_t end = std::min(size_-1, i+increment);
+        const size_t s = end - i + 1;
+        getRequest.SetRange(S3InterfaceUtils::ByteRange(i, end).toRangeString());
+        outstandingRequests[i / increment] = false;
+        client.GetObjectAsync(getRequest, [this, i, s](
+                const Aws::S3Crt::S3CrtClient*,
+                const Aws::S3Crt::Model::GetObjectRequest&,
+                Aws::S3Crt::Model::GetObjectOutcome outcome,
+                const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
+            outcome.GetResult().GetBody().read(result->data() + i, s);
+            outstandingRequests[i / increment] = true;
+            outstandingRequests[i / increment].notify_all();
+        });
+    }
 public:
     explicit PrefetchFile(const std::string& p) : key(p + ".parquet"){
         std::cout << "created prefetch file" << std::endl;
-    }
-
-    size_t size() override {
         Aws::S3Crt::Model::HeadObjectRequest request;
         request.SetBucket(bucket);
         request.SetKey(key);
-        const size_t size = client.HeadObject(request).GetResult().GetContentLength();
-        assert(size < increment * 1024);
-        result->resize(size);
-        for (size_t i = 0; i<size; i+= increment){
-           Aws::S3Crt::Model::GetObjectRequest getRequest;
-           getRequest.SetBucket(bucket);
-           getRequest.SetKey(key);
-           const size_t end = std::min(size-1, i+increment);
-           const size_t s = end - i + 1;
-           getRequest.SetRange(S3InterfaceUtils::ByteRange(i, end).toRangeString());
-           outstandingRequests[i / increment] = false;
-           client.GetObjectAsync(getRequest, [this, i, s](
-                   const Aws::S3Crt::S3CrtClient*,
-                   const Aws::S3Crt::Model::GetObjectRequest&,
-                   Aws::S3Crt::Model::GetObjectOutcome outcome,
-                   const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
-               outcome.GetResult().GetBody().read(result->data() + i, s);
-               outstandingRequests[i / increment] = true;
-           });
+        size_ = client.HeadObject(request).GetResult().GetContentLength();
+        assert(size_ < increment * 1024);
+        result->resize(size_);
+        makeRequest(size_ / increment * increment);
+        for (size_t i = 0; i+increment<size_; i+= increment){
+            makeRequest(i);
         }
-        return size;
+    }
+
+    size_t size() override {
+        return size_;
     }
 
     std::shared_ptr<std::string> getRange(S3InterfaceUtils::ByteRange byteRange) override {
