131d790a6fe30946225ddc7703dd5ae35f1645f5
pascalginter <ginpa2000@gmail.com>
2025-04-01 14:47:30 +0200

    Queryable Postgres backed files

---
+++ src/virtualization/PostgresBufferedFile.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#include <iostream>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <pqxx/pqxx>
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/arrow/writer.h>
+
+#include "VirtualizedFile.hpp"
+
+class PostgresBufferedFile final : public VirtualizedFile {
+    static thread_local std::shared_ptr<std::string> result;
+    static thread_local pqxx::connection conn;
+    std::shared_ptr<arrow::Buffer> buffer;
+
+    std::size_t size_;
+public:
+    explicit PostgresBufferedFile(const std::string& p) {
+        std::cout << "created postgres buffered file" << std::endl;
+        pqxx::work tx(conn);
+
+        // Prepare buffers to load data
+        std::unordered_map<int, std::shared_ptr<arrow::Buffer>> rebuildFiles;
+        for (const auto& [fileId, size] :
+                 tx.query<int, int>("SELECT file_id, size FROM BufferedFiles WHERE finalized")){
+            rebuildFiles[fileId] = arrow::AllocateBuffer(size).ValueOrDie();
+        }
+
+        // Populate buffers with data
+        for (const auto& [fileId, fileOffset, size, content] :
+                tx.query<int, int, int, std::basic_string<std::byte>>("Select file_id, file_offset, size, content FROM bufferedView")) {
+            std::cout << fileId << " " << size << std::endl;
+            std::cout << "a " << content.c_str() << std::endl;
+            memcpy(rebuildFiles[fileId]->mutable_data() + fileOffset, content.data(), size);
+        }
+
+        // Read buffers into arrow
+        std::vector<std::shared_ptr<arrow::Table>> tables;
+        for (const auto& singleBuffer : rebuildFiles){
+            // Read parquet into arrow table
+            const auto buffer_reader = std::make_shared<arrow::io::BufferReader>(singleBuffer.second);
+            std::unique_ptr<parquet::arrow::FileReader> parquet_reader;
+            const auto status = parquet::arrow::OpenFile(buffer_reader, arrow::default_memory_pool(), &parquet_reader);
+            if (!status.ok()) {
+                std::cout << status << std::endl;
+            }
+            std::cout << parquet_reader << std::endl;
+            std::shared_ptr<arrow::Table> table;
+            PARQUET_THROW_NOT_OK(parquet_reader->ReadTable(&table));
+            std::cout << table->ToString() << std::endl;
+            tables.push_back(table);
+        }
+
+        const auto combinedTable = arrow::ConcatenateTables(tables).ValueOrDie();
+
+        // Write table to buffer
+        const auto out = arrow::io::BufferOutputStream::Create().ValueOrDie();
+        PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(*combinedTable, arrow::default_memory_pool(), out));
+        buffer = out->Finish().ValueOrDie();
+        std::cout << (*buffer)[buffer->size() - 4] << (*buffer)[buffer->size() - 3] << (*buffer)[buffer->size() - 2] << (*buffer)[buffer->size() - 1] << std::endl;
+        std::cout << "size" << buffer->size() << " " << buffer->size() / 1024 << std::endl;
+        size_ = buffer->size();
+    }
+
+    size_t size() override {
+        return size_;
+    }
+
+    std::shared_ptr<std::string> getRange(const S3InterfaceUtils::ByteRange byteRange) override {
+        result->resize(byteRange.size());
+        memcpy(result->data(), buffer->data() + byteRange.begin, byteRange.size());
+        return result;
+    }
+};
